5324867754521dbbe6a3581b8bdfec36d3ec92cc
Nate Pruitt <nathaniel.n.pruitt@gmail.com>
2026-07-11 18:07:12 -0700

    Add useRef in auth context to avoid refresh double tap.

--- apps/frontend/src/components/AuthContext.tsx
+++ apps/frontend/src/components/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+import { createContext, useContext, useEffect, useRef, useState, type PropsWithChildren } from "react";
 import type { UserRequestDTO } from "../types/UserDTO";
 import { jwtDecode } from "jwt-decode";
 import { type JwtData } from "../types/Jwt";
@@ -24,28 +24,33 @@ export function AuthProvider({ children }: PropsWithChildren) {
     const [expiry, setExpiry] = useState<Date>(new Date());
     const [loading, setLoading] = useState<boolean>(true);
 
-    useEffect(() => {
-        setLoading(true)
-        fetch(BASE_URL + "/api/v0/refresh", {
-            method: "POST",
-            credentials: "include",
-        }).then(async (resp) => {
-            if(resp.ok) {
-                const authHeader = resp.headers.get("Authorization")
-                const token = authHeader && authHeader.replace(/^Bearer\s+/i, '');
-                
-                if(!token) {
-                    throw new Error("Token not included in response.")
-                }
+    const refreshSent = useRef(false);
 
-                let bodyData: UserRequestDTO = await resp.json()
-                setUsername(bodyData.data.username)
-                setJWT(token)
-                
-                const jwtData = jwtDecode<JwtData>(token)
-                setExpiry(new Date(jwtData.exp * 1000))
-            }
-        }).finally(() => setLoading(false))
+    useEffect(() => {
+        if(!refreshSent.current) {
+            refreshSent.current = true;
+            setLoading(true)
+            fetch(BASE_URL + "/api/v0/refresh", {
+                method: "POST",
+                credentials: "include",
+            }).then(async (resp) => {
+                if(resp.ok) {
+                    const authHeader = resp.headers.get("Authorization")
+                    const token = authHeader && authHeader.replace(/^Bearer\s+/i, '');
+                    
+                    if(!token) {
+                        throw new Error("Token not included in response.")
+                    }
+
+                    let bodyData: UserRequestDTO = await resp.json()
+                    setUsername(bodyData.data.username)
+                    setJWT(token)
+                    
+                    const jwtData = jwtDecode<JwtData>(token)
+                    setExpiry(new Date(jwtData.exp * 1000))
+                }
+            }).finally(() => setLoading(false))
+        }
     }, []);
     
     return (
