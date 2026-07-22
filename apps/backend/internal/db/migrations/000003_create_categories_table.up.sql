ffda6eadb83ee07ecd52c5e48fdc0f6d2660fe9c
Nate Pruitt <nathaniel.n.pruitt@gmail.com>
2026-07-21 21:06:58 -0700

    Add cascade on delete to the project, category, and card tables to avoid zombie records.

--- apps/backend/internal/db/migrations/000003_create_categories_table.up.sql
+++ apps/backend/internal/db/migrations/000003_create_categories_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS categories (
     id UUID PRIMARY KEY,
-    project_id UUID REFERENCES projects(id),
+    project_id UUID REFERENCES projects(id) ON DELETE CASCADE,
     category_name varchar(255) NOT NULL,
     index INT NOT NULL
 );
--- apps/backend/internal/db/migrations/000004_create_cards_table.up.sql
+++ apps/backend/internal/db/migrations/000004_create_cards_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS cards (
     id UUID PRIMARY KEY,
     title varchar(255) NOT NULL,
     content varchar,
-    category_id UUID REFERENCES categories(id),
+    category_id UUID REFERENCES categories(id) ON DELETE CASCADE,
     finished BOOL NOT NULL
 );
